0997207a01f93cd475e9284ce31d38ce03faa7a3
JungYeonHwi <64068511+JungYeonHwi@users.noreply.github.com>
2020-12-15 07:17:27 +0900

    Update animal.java

--- animal.java
+++ animal.java
@@ -1,150 +1,150 @@
-package number;
+package animal;
 
 import java.util.*;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class number implements ActionListener 
-{
-       JFrame numberFrame;
+public class animal implements ActionListener{
+	static String[] images = {
+			"animal_image/BSM20.jpg", "animal_image/CAO18.jpg", "animal_image/CCO14.jpg", "animal_image/DOS22.jpg",
+			"animal_image/DSC18.jpg", "animal_image/MDS16.jpg", "animal_image/PCC8.jpg", "animal_image/SCP14.jpg"
+	};
+	
+	JFrame legFrame;
+	
+	JPanel legPanel, imagePanel;
+	
+	JLabel guessLabel, titleLabel, scoreLabel, textLabel, imageLabel;
+	
+	ImageIcon imageIcon;
+	
+	JButton scoreButton;
+	
+	JTextField guessText;
+	
+	int i;
+	
+	Random rand = new Random();
+    int index = rand.nextInt(8)+1;
+    
+    int guess;
+    int answer;
+    int score = 30;
+    
+    public animal()
+	{
+		
+		legFrame = new JFrame();
+		legFrame.setTitle("동물 다리 개수 맞추기");
 
-       JPanel numberPanel;
+		legFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		legPanel = new JPanel();
+		legPanel.setLayout(new GridLayout(5,0));
+		
+        legFrame.getContentPane().add(legPanel, BorderLayout.CENTER);
 
-       JTextField guessText;
+		
+		displayWidgets();
+	
+		legFrame.pack();
+        legFrame.setVisible(true);
+	}
+    
+    public void displayWidgets() 
+    {
+    	titleLabel = new JLabel("동물의 총 다리 개수 맞추기", SwingConstants.CENTER);
+	    titleLabel.setFont(new Font("굴림", Font.BOLD, 30));
+			
+	    guessLabel = new JLabel("다리 개수를 입력하세요", SwingConstants.CENTER);
+	    guessLabel.setFont(new Font("굴림", Font.CENTER_BASELINE, 20));
+	     
+	    textLabel = new JLabel(" ", SwingConstants.CENTER);
+	    
+	    guessText = new JTextField();
+	    guessText.setHorizontalAlignment(JTextField.CENTER);
+	    guessText.addActionListener(this);
 
-       JLabel rangeLabel, guessLabel, winLabel, scoreLabel;
-       
-       JButton scoreButton;
+	    imageIcon = new ImageIcon(images[index]);
+	    imageLabel = new JLabel(imageIcon);
+	    
+	    ImageIcon scoreicon = new ImageIcon("socreicon.png");
 
-       Random rand = new Random();
-       int Answer = rand.nextInt(30)+1;
-       int guess;
-       
-       int count = 0;
-       boolean win = false;
-       int score;
+        scoreButton = new JButton("점수를 확인하세요");
+        scoreButton.setPressedIcon(scoreicon);
+        
+        scoreButton.addMouseListener(new MyMouseListener());
 
-      public number() 
-      {
-           numberFrame = new JFrame();
-           numberFrame.setTitle("Number Guess");
-           
-           numberFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);;
-           
-           numberPanel = new JPanel();
-           numberPanel.setLayout(new GridLayout(5,0));
-           
-           numberFrame.getContentPane().add(numberPanel, BorderLayout.CENTER);
+	    legPanel.add(titleLabel);
+	    legPanel.add(guessLabel);
+	    legPanel.add(imageLabel);
+	    legPanel.add(guessText);
+	    legPanel.add(scoreButton);
+	    
+	    if (index == 0)	answer = 20;
+	    if (index == 1) answer = 18;
+	    if (index == 2) answer = 14;
+	    if (index == 3) answer = 22;
+	    if (index == 4) answer = 18;
+	    if (index == 5) answer = 16;
+	    if (index == 6) answer = 8;
+	    if (index == 7) answer = 14;
 
-           displayWidgets();
+    }
+    
+    public void actionPerformed(ActionEvent event) 
+    {        
+    	guess = Integer.parseInt(guessText.getText());
 
-           numberFrame.pack();
-           numberFrame.setVisible(true);  
-       }
-
-       public void displayWidgets() 
-       {  
-
-           guessText = new JTextField();
-           guessText.setHorizontalAlignment(JTextField.CENTER);
-           
-           rangeLabel = new JLabel("1~30 사이 숫자 중 하나를 맞추기", SwingConstants.CENTER);
-           rangeLabel.setFont(new Font("굴림", Font.BOLD, 30));
-           
-           guessLabel = new JLabel("추측된 숫자를 입력하세요", SwingConstants.CENTER);
-           guessLabel.setFont(new Font("굴림", Font.CENTER_BASELINE, 20));
-           
-           winLabel = new JLabel(" ", SwingConstants.CENTER);
-           
-           ImageIcon scoreicon = new ImageIcon("socreicon.png");
-
-           scoreButton = new JButton("점수를 확인하세요");
-           scoreButton.setPressedIcon(scoreicon);
-           
-           scoreButton.addMouseListener(new MyMouseListener());
-
-           guessText.addActionListener(this);
-
-           numberPanel.add(rangeLabel);
-           numberPanel.add(guessLabel);
-           numberPanel.add(guessText);
-           numberPanel.add(winLabel);
-           numberPanel.add(scoreButton);
-          
-       }
-
-       public void actionPerformed(ActionEvent event) 
-       {        
-           guess = Integer.parseInt(guessText.getText());
-
-           if ( guess == Answer)
-           {
-               count ++;
-               winLabel.setText("정답입니다");
-               winLabel.setFont(winLabel.getFont().deriveFont(20.0f));
-               scoreButton.setEnabled(true);
-           }
-           else if ( guess < Answer)
-           {
-               winLabel.setText("너무 작은 숫자입니다");
-               winLabel.setFont(winLabel.getFont().deriveFont(20.0f));
-               winLabel.setForeground(Color.blue);
-               count ++;
-               scoreButton.setEnabled(false);
-           }
-           else if ( guess > Answer)
-           {
-               winLabel.setText("너무 큰 숫자입니다");
-               winLabel.setFont(winLabel.getFont().deriveFont(20.0f));
-               winLabel.setForeground(Color.blue);
-               count ++;
-               scoreButton.setEnabled(false);
-           }
-           
-           if (count > 30) score = 10;
-           if (count < 20 && count > 10) score = 20;
-           if (count < 10 && count > 5) score = 30;
-           if (count < 5 && count > 1) score = 40;
-           if (count == 1) score = 50;
-           
-           JOptionPane.showMessageDialog(null, count, "시도 횟수", JOptionPane.INFORMATION_MESSAGE);
-
-       }
-       
-       class MyMouseListener implements MouseListener{
-           @Override
-           public void mouseClicked(MouseEvent e) {
-        	   if (e.getSource() == scoreButton)
-        		   scoreButton.setText("점수는 " + score);
+        if (guess == answer)
+        {
+            score++;
+            JOptionPane.showMessageDialog(null, "정답입니다", "", JOptionPane.INFORMATION_MESSAGE);
+            scoreButton.setEnabled(true);
+        }
+        
+        else
+        {
+        	score--;
+        	JOptionPane.showMessageDialog(null, "오답입니다", "", JOptionPane.INFORMATION_MESSAGE);
+        	scoreButton.setEnabled(false);
+        }
+	}
+    
+    class MyMouseListener implements MouseListener{
+        @Override
+        public void mouseClicked(MouseEvent e) {
+     	   if (e.getSource() == scoreButton)
+     		   scoreButton.setText("점수는 " + score);
 			}
 
-           @Override
-           public void mouseEntered(MouseEvent arg0) {
+        @Override
+        public void mouseEntered(MouseEvent arg0) {
 
-           }
+        }
 
-           @Override
-           public void mouseExited(MouseEvent arg0) {
+        @Override
+        public void mouseExited(MouseEvent arg0) {
 		
 			
-           }
+        }
 
-           @Override
-           public void mousePressed(MouseEvent arg0) {
+        @Override
+        public void mousePressed(MouseEvent arg0) {
 			
 			
-           }
+        }
 
-           @Override
-           public void mouseReleased(MouseEvent arg0) {
+        @Override
+        public void mouseReleased(MouseEvent arg0) {
 			
 			
-           }
-       }
-       
-       public static void main(String[] args)
-       {
-           number frame = new number();
-       } 
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        animal frame = new animal();
+    } 
 }
